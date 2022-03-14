496087d1a53328896285deafcc93bc8450308278
Bryce Martin <bryce.martin@ttpsolutions.com>
2022-03-14 13:31:22 -0500

    Create backbone script/model/collection/view

--- backbone_scripts/devs.backbone.js
+++ backbone_scripts/devs.backbone.js
@@ -16,20 +16,45 @@
     
     //backbone dev collection
     var Devs = Backbone.Collection.extend({
-        model: dev
+        model: Dev
     });
     
     //two devs on this project
     
-    devs.add(new Dev({
-        name: "Bryce Martin",
-        job_title: "Web Developer",
-        company: "TTP Solutions"
-    }));
-    
-    devs.add(new Dev({
-        name: "Nick Ehrhart",
-        job_title: "Web Developer",
-        company: "TTP Solutions"
-    }));
+    var developers = new Devs([
+        new Dev ({
+            name:"Bryce Martin",
+            job_title: "Web Developer",
+            company: "TTP Solutions"
+        }),
+        new Dev ({
+            name: "Nick Ehrhart",
+            job_title: "Web Developer",
+            company: "TTP Solutions"
+        })
+    ]);
+
+    //dev view
+    var DevView = Backbone.View.extend({
+        render: function() {
+            var template = _.template($("#devTemplate").html())
+            var html = template(this.model.toJSON());
+            this.$el.html(html);
+
+            return this;
+        }
+    });
+
+    var DevsView = Backbone.View.extend({
+        render: function() {
+            var self = this;
+            this.model.each(function(dev) {
+                var devview = new DevView({model: dev});
+                self.$el.append(devview.render().$el)
+            });
+        }
+    });
+
+    var template = new DevsView({ el: ".container", model: developers });
+    template.render();
 })();
